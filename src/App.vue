400e5fbf427ab8a97d70cf65b3c2cade1a59a1f7
LionelSebastian <lionel.sebastian.cifo@gmail.com>
2022-12-08 14:23:44 +0100

    Added=> Second iteration done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
-
+    <table>
+      <thead>
+        <tr>
+          <th>Foto</th>
+          <th>Nombre</th>
+          <th>Popularidad</th>
+          <th>Won an Oscar</th>
+          <th>Won an EMY</th>
+        </tr>    
+      </thead>
+      <tbody>
+        <tr v-for="contact in contacts">         
+          <th><img width="50" :src="contact.pictureUrl" alt="foto de perfil"></th>
+          <th>{{ contact.name }}</th>
+          <th>{{ contact.popularity }} </th>
+          <th> <img v-if="item." src="./" alt=""></th>
+        </tr>
+      </tbody>
+   </table>
 </template>
 
-<script setup>
-import  contacts from `./contacts.json`;
+<script>
+import CONTACTS from "./contacts.json";
 
 export default{
-  contacts=[contracts]
+  components:{
+
+  },
+  data(){
+    return{
+      contacts:CONTACTS.slice(0,5),
+    };
+  },
 };
 </script>
 
